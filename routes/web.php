fcdc001a7e3cf251ee1ed1cc137a29cc68ce4977
olmedo <olmedo,bdp@gmail.com>
2020-04-27 11:50:56 -0500

    proyecto base

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'TestController@welcome');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
