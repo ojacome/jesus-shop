3b53fc4a3ce31ff94afded35f15eb3d9648311e0
olmedo <olmedo,bdp@gmail.com>
2020-04-29 12:58:37 -0500

    se completa integracion de la plantilla tim  creative

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                                         <a  href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
+                                            {{ __('Cerrar Sesion') }}
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
