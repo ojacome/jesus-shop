457cf10f788e93a7a4f8b8609389166705a77918
olmedo <olmedo,bdp@gmail.com>
2020-05-01 18:06:26 -0500

    registro datos mediante realaciones de factories

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,8 +14,25 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Category::class, 5)->create();
+        /* factory(Category::class, 5)->create();
         factory(Product::class, 100)->create();//crea 100 productos 
-        factory(ProductImage::class, 200)->create();
+        factory(ProductImage::class, 200)->create(); */
+
+        $categories = factory(Category::class, 5)->create();           
+        $categories->each(function ($category) {
+            $products = factory(Product::class, 20)->make();
+            $category->products()->saveMany($products);
+
+            $products->each(function($p) {
+                $images = factory(ProductImage::class, 5)->make();
+                $p->images()->saveMany($images);
+            });
+        });
+
+        /* users = factory(App\User::class, 3)
+           ->create()
+           ->each(function ($user) {
+                $user->posts()->save(factory(App\Post::class)->make());
+            }); */
     }
 }
