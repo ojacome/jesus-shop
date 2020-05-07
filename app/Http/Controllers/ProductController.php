82d4360dadab49cd1d506851309edbdde8cbf711
olmedo <olmedo,bdp@gmail.com>
2020-05-07 18:39:08 -0500

    pagina de producto

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function show($id){
         
-        return "mostrar datos";
+        $product = Product::find($id);
+        $images = $product->images;
+        
+        $imagesLeft = collect();
+        $imagesRight = collect();
+        foreach($images as $key => $image){
+            if($key%2==0)
+                $imagesLeft->push($image);
+            else
+                $imagesRight->push($image);
+        }
+
+        return view('products.show')->with(compact('product', 'imagesLeft', 'imagesRight'));
     }
 }
